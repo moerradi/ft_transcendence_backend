6e12b349273d08bc939e973addc3938ca38c9abb
moerradi <erradi1337@gmail.com>
2022-12-19 03:44:21 +0100

    small backup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // get config service
+  const configService = app.get(ConfigService);
+  // disable cors for dev purposes
+  if (configService.get('NODE_ENV') === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({
+      origin: configService.get('CORS_ORIGIN'),
+      credentials: true,
+    });
+  }
+  // add whitelist to validation pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // configruate swagger
+  const options = new DocumentBuilder()
+    .setTitle('Transcendance API')
+    .setDescription('This is the API for the Transcendance project.')
+    .setVersion('1.0')
+    .addTag('main')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  const port = configService.get('PORT') || 4242;
+  await app.listen(port);
 }
 bootstrap();
